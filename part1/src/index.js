5becc2067a2f14f6d9c67f1f4fde10efb8913c47
Pablo <pnunfe@gmail.com>
2023-07-11 12:15:40 +0200

    This is "Introduction in React"

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  // console.log('Hello from component')
+  const now = new Date()
+  const name = "Jesus"
+  const age = 10
+
+  return (
+    <>
+      <h1>{now.toString()}</h1>
+      <Hello name="Pablo" age={20 + 15} />
+      <Hello name="Carla" age={age} />
+      <Hello name={name} age={38} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root')) 
