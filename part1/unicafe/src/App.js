9bacc40b091c7c6a148f06322794ed27dcf33689
Pablo <pnunfe@gmail.com>
2023-08-10 16:35:07 +0200

    1.11 with bug?

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 
-const Statistics = (props) => {
-  const { good, neutral, bad } = props;
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
-  const average = (good - bad) / 3;
+  const average = (good - bad) / total;
   const percentageOfGood = total !== 0 ? (good / total) * 100 : 0;
 
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {total}</p>
-      <p>Average: {average} </p>
-      <p>Positive: {percentageOfGood}%</p>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="All" value={total} />
+      <StatisticLine text="Average" value={average} />
+      <StatisticLine text="Positive" value={`${percentageOfGood}%`} />
     </div>
   );
 };
@@ -24,20 +34,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const feedbackGiven = good + neutral + bad;
-
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <table>
+        <tbody>
+          <h1>Give feedback</h1>
+          <Button handleClick={() => setGood(good + 1)} text="Good" />
+          <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+          <Button handleClick={() => setBad(bad + 1)} text="Bad" />
 
-      {feedbackGiven > 0 ? (
-        <Statistics good={good} neutral={neutral} bad={bad} />
-      ) : (
-        <p>No feedback given</p>
-      )}
+          {good + neutral + bad > 0 ? (
+            <Statistics good={good} neutral={neutral} bad={bad} />
+          ) : (
+            <p>No feedback given</p>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
