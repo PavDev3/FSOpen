46df5bba91982b3ae3bd05b87cb351e998f39621
Pablo <pnunfe@gmail.com>
2023-08-22 15:22:29 +0200

    part3 before exercices1

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -19,9 +21,8 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         date: "2019-05-30T19:20:14.298Z",
         important: true
-    }
+    },
 ]
-
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -29,6 +30,48 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    //Tienes que pasar el ID a numbers porque lo entiende como string
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+
+    if (!note || !note.content) {
+        return response.status(404).json({
+            error: 'Note.content is missing'
+        })
+    }
+
+    const ids = notes.map(note => note.id)
+    const maxId = Math.max(...ids)
+    const newNote = {
+        id: maxId + 1,
+        content: note.content,
+        important: typeof note.important !== 'undefined' ? note.important : false,
+        date: new Date().toISOString(),
+    }
+
+    notes = [...notes, newNote]
+
+    response.status(201).json(newNote)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+
 })
 
 const PORT = 3001
